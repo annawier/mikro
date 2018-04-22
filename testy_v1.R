030a67c7f1a71d25633a594ae1a4a28289d9b8d0
KarinaOstoj <38165695+KarinaOstoj@users.noreply.github.com>
2018-04-22 11:05:47 +0200

    Add files via upload

---
+++ testy_v1.R
@@ -0,0 +1,189 @@
+################################################################################
+# MODELOWANIE
+################################################################################
+
+# rm(list = ls())
+
+require(readr); require(MASS); require(ordinal); require(dplyr); require(nnet)
+require(brant); require(lmtest)
+# require(erer)
+
+soccer_df_raw <- read_csv(paste0(path, "sd_glm_merged_3.csv"))
+colnames(soccer_df_raw)
+# soccer_df_raw[, 1] <- NULL # X1 col 
+
+
+
+# USUWAM BRAKI DANYCH - dwie grupy 23524 & 12118
+sapply(soccer_df_raw, function(x) any(is.na(x)))
+soccer_df_raw_NAs_sum <- sapply(soccer_df_raw, function(x) sum(is.na(x)))
+soccer_df_raw_NAs <- sapply(soccer_df_raw, function(x) is.na(x))
+na_id_12118 <- which(soccer_df_raw_NAs_sum == 12118)
+na_id_23524 <- which(soccer_df_raw_NAs_sum == 23524)
+
+all(soccer_df_raw_NAs[, na_id_12118[1]] == soccer_df_raw_NAs[, na_id_12118[-1]])
+# braki danych w tych samych miejscach
+
+indeks <- !soccer_df_raw_NAs[, na_id_12118[1]]
+
+# SOCCER DATAFRAME
+soccer_df <- soccer_df_raw[indeks, ]
+
+# braki danych w zmiennych 0_ ... (np. NA NA, NA Cover, Cover NA, ...)
+sapply(soccer_df[, grep("O_", colnames(soccer_df))], 
+       function(x) unique(x))
+sapply(soccer_df[, grep("O_", colnames(soccer_df))], 
+       function(x) sum(grepl("NA", x)))
+soccer_df[, grep("O_", colnames(soccer_df))] <- 
+  sapply(soccer_df[, grep("O_", colnames(soccer_df))], 
+         function(x) {
+           x <- ifelse(grepl(" NA", x), NA, x)
+           ifelse(grepl("NA ", x), NA, x)
+         })
+
+# rm(list = setdiff(ls(), "soccer_df"))
+
+sapply(soccer_df, function(x) sum(is.na(x)))
+# goal, shoton, shotoff, foulcommit, card, cross, corner, possession - du¿o NAs
+
+soccer_df$buildUpPlayDribbling <- NULL # b du¿o NAs
+
+
+
+################################################################################
+# ZMIENNE OBJAŒNIAJ¥CE
+colnames(soccer_df)
+
+soccer_df1 <- soccer_df %>%
+  select(result, 
+         season, stage, home_or_away, goals, # shoton, shotoff, # match data
+         # foulcommit, card, cross, corner, possession, # match data
+         colnames(soccer_df)[grep("Class", colnames(soccer_df))])
+
+colnames(soccer_df1)
+
+sapply(soccer_df1, function(x) table(x))
+sapply(soccer_df1[, -1], function(x) table(soccer_df1$result, x))
+
+soccer_df1 <- data.frame(sapply(soccer_df1, function(x) factor(x)))
+str(soccer_df1)
+
+# referencje
+soccer_df1$home_or_away_ref <- relevel(soccer_df1$home_or_away, ref = "away")
+
+
+
+################################################################################
+# MODEL
+
+soccer_df1$result_ref <- relevel(soccer_df1$result, ref = "won")
+
+# soccer_model <- clm(result ~ home_or_away, data = soccer_df1)
+# summary(soccer_model)
+# 
+# soccer_model <- multinom(result ~ home_or_away, data = soccer_df1, Hess = T)
+# summary(soccer_model)
+
+# soccer_model <- polr(result_ref ~ home_or_away, data = soccer_df1, Hess = T)
+# summary(soccer_model)
+
+soccer_model <- polr(result ~ home_or_away 
+                     + buildUpPlayPassingClass
+                     + chanceCreationShootingClass
+                     + defenceDefenderLineClass
+                     
+                     , data = soccer_df1, Hess = T)
+summary(soccer_model)
+
+coef_table <- coef(summary(soccer_model))
+coef_table <- cbind(coef_table,
+                    "p value" = pnorm(abs(coef_table[, "t value"]), lower.tail = FALSE) * 2)
+
+exp_coef <- exp(coef(soccer_model))
+l
+
+
+# TESTY
+brant(soccer_model)
+
+soccer_model_null <- polr(result ~ +1
+                          , data = soccer_df1, Hess = T)
+lrtest(soccer_model, soccer_model_null)
+
+#########################################################################################
+# TESTY c.d.
+#########################################################################################
+
+for(i in team_attr_list) {
+  sd_glm_merged_2[[i]] <- as.factor(sd_glm_merged_2[[i]])
+  sd_glm_merged_3[[i]] <- as.factor(sd_glm_merged_3[[i]])
+}
+sd_glm_merged_2$result <- as.factor(sd_glm_merged_2$result)
+sd_glm_merged_3$result <- as.factor(sd_glm_merged_3$result)
+
+
+soccer_model  <- 
+  polr(result ~ home_or_away
+       + buildUpPlaySpeedClass
+       + buildUpPlayDribblingClass
+       + buildUpPlayPassingClass
+       + buildUpPlayPositioningClass
+       + chanceCreationPassingClass
+       + chanceCreationCrossingClass
+       + chanceCreationShootingClass
+       + chanceCreationPositioningClass
+       + defencePressureClass
+       + defenceAggressionClass
+       + defenceTeamWidthClass
+       + defenceDefenderLineClass
+       , data = sd_glm_merged_2
+       , Hess=TRUE)
+
+summary(soccer_model)
+
+##checklist:
+#1. laczna istotnosc wszystkich zmiennych - test ilorazu wiarygodnosci
+#test moze sluzyc do porownywania modeli zagniezdzonych (forward/backward variable selection)
+#test ten moze sluzyc do oceny homogenicznosci grupy
+#zrodlo: https://stats.stackexchange.com/questions/7720/how-to-understand-output-from-rs-polr-function-ordered-logistic-regression
+pchisq(deviance(soccer_model), df.residual(soccer_model))
+#UWAGA! Podejrzany wynik... 
+
+
+#2. pseudo R2
+# https://www.rdocumentation.org/packages/DescTools/versions/0.99.19/topics/PseudoR2
+install.packages("DescTools")
+library(DescTools)
+R2s<- PseudoR2(soccer_model, which="all")
+
+# - McKelvey and Zavoina - nie policzyl sie
+
+# - McFadden
+
+# - McFadden adj
+
+# - Maximum Likelihood R2
+
+# - Cragg and Uhler
+
+R2s <- as.data.frame(t(R2s))
+R2s
+colnames(R2s)
+
+2*(getElement(R2s, "logLik")-getElement(R2s, "logLik0"))
+
+
+
+#3. kryteria informacyjne AIC BIC
+# AIC - output polr
+# AIC corrected
+R2s<- PseudoR2(soccer_model, which="all")
+
+
+#zdolnosc predykcyjna modelu
+# count R2
+# count R2 Adj
+
+
+
+
